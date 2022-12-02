5a58bc43b438e48a6393d0eff6a60d6447ab610d
Max Shvagerus <mshvagerus@gmail.com>
2022-12-03 01:07:00 +0300

    game test

---
+++ src/main/java/ru/netology/manager/Game.java
@@ -0,0 +1,42 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Player;
+import ru.netology.exception.NotRegisteredException;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    ArrayList<Player> players = new ArrayList<>();
+
+    public void register(Player player) {
+        players.add(player);
+    }
+
+    public int round(String playerName1, String playerName2) {
+        Player player1 = findByName(playerName1);
+        Player player2 = findByName(playerName2);
+        if (player1 == null) {
+            throw new NotRegisteredException(playerName1);
+        }
+        if (player2 == null) {
+            throw new NotRegisteredException(playerName2);
+        }
+        if (player1.getStrength() > player2.getStrength()) {
+            return 1;
+        }
+        if (player1.getStrength() < player2.getStrength()) {
+            return 2;
+        }
+        return 0;
+    }
+
+    public Player findByName(String name) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
+                return player;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/ru/netology/manager/GameTest.java
@@ -0,0 +1,63 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Player;
+import ru.netology.exception.NotRegisteredException;
+
+import java.security.PublicKey;
+
+public class GameTest {
+    Game game = new Game();
+
+    Player player1 = new Player(1, "Player 1", 110);
+    Player player2 = new Player(2, "Player 2", 105);
+    Player player3 = new Player(3, "Player 3", 100);
+    Player player4 = new Player(4, "Player 4", 120);
+    Player player5 = new Player(5, "Player 5", 105);
+
+    @BeforeEach
+    public void setup() {
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+        game.register(player4);
+        game.register(player5);
+    }
+
+    @Test
+    public void shouldWinFirstPlayer() {
+
+        int expected = 1;
+        int actual = game.round("Player 2", "Player 3");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldWinSecondPlayer() {
+
+        int expected = 2;
+        int actual = game.round("Player 1", "Player 4");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDrawnGame() {
+
+        int expected = 0;
+        int actual = game.round("Player 2", "Player 5");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotRegistered() {
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Player 2", "Player 6");
+        });
+    }
+}
